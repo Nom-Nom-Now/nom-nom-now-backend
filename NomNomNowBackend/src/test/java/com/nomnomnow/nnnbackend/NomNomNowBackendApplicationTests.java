03fb0c89503854ff25594225ed7c92980c3f8db0
Rafael Till <tillrafael@icloud.com>
2025-10-01 11:30:03 +0200

    Initialize NomNomNowBackend project with Spring Boot, Maven wrapper, and standard project setup files.

---
+++ NomNomNowBackend/src/test/java/com/nomnomnow/nnnbackend/NomNomNowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nomnomnow.nnnbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomNomNowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
