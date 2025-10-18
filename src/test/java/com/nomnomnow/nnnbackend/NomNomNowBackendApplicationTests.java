a3d76913ab1190b4e87df6fc23375185c451eeda
Rafael Till <tillrafael@icloud.com>
2025-10-18 14:26:19 +0200

    add demo testcontainer database setup

--- src/test/java/com/nomnomnow/nnnbackend/NomNomNowBackendApplicationTests.java
+++ src/test/java/com/nomnomnow/nnnbackend/NomNomNowBackendApplicationTests.java
@@ -2,12 +2,29 @@ package com.nomnomnow.nnnbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class NomNomNowBackendApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Container
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:16-alpine");
+
+    @DynamicPropertySource
+    static void dbProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+        registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
     }
+
+
+    @Test
+    void contextLoads() {}
 
 }
