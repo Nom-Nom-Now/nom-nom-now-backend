ce7451b7af29b37af20ac9063bced6ee933df67a
Rafael Till <tillrafael@icloud.com>
2026-04-02 09:59:14 +0200

    feat(recipes): enhance recipe retrieval with batch size and pagination support

--- src/main/java/com/nomnomnow/nnnbackend/repository/RecipeRepository.java
+++ src/main/java/com/nomnomnow/nnnbackend/repository/RecipeRepository.java
@@ -1,9 +1,15 @@
 package com.nomnomnow.nnnbackend.repository;
 
 import com.nomnomnow.nnnbackend.entity.Recipe;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
+    @Override
+    @EntityGraph(attributePaths = {"components.ingredient"})
+    Page<Recipe> findAll(Pageable pageable);
 }
