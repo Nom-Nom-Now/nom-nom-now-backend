145190bbba3ead0114315dc2e778b08c5cf73ff3
Robin0106 <fischerrobin58@gmail.com>
2026-04-10 19:09:49 +0200

    feat:Fixed Typo in flyway migration

--- flyway/sql/V5__migrate_categories_to_enum.sql
+++ flyway/sql/V5__migrate_categories_to_enum.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS app.recipe_category;
 DROP TABLE IF EXISTS app.category;
 
 ALTER TABLE app.recipe
-    ADD COLUMN IF NOT EXISTS catagories TEXT;
+    ADD COLUMN IF NOT EXISTS categories TEXT;
 
 CREATE INDEX IF NOT EXISTS idx_recipe_categories
     ON app.recipe USING GIN (to_tsvector('simple', categories));
